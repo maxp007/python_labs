08211387718ed5fb8fe873f96578d917aa5cd188
maxp007 <vpupkin9208@gmail.com>
2017-11-24 15:16:52 +0300

    Added review lab7

--- lab7/my_app/admin.py
+++ lab7/my_app/admin.py
@@ -1,11 +1,8 @@
 from django.contrib import admin
 from my_app.models import OrderModel, ComputerModel, CustomerModel
 
-
-
 class CustomerFilter(admin.ModelAdmin):
     pass
-
 
 class CustomerAdmin(admin.ModelAdmin):
     exclude = ('password', )
